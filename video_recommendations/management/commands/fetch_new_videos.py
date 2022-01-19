a4ca3f644dedd5ec1f8904ea787f462318e1bc25
Abhineeth Adiraju <abhineeth00@gmail.com>
2022-01-18 22:40:48 -0800

    Setup video fetching (#2)
    * Setup django command for fetching videos so it can be updated to add new videos into the DB
    
    * Added more fields to video resource
    
    * Added extra fields to video model as neeeded and added code to save youtube videos to the database
    
    * Added pandas as a requirement and finalized fetch_new_videos script to work for all categories
    
    * flake8

---
+++ video_recommendations/management/commands/fetch_new_videos.py
@@ -0,0 +1,67 @@
+from django.core.exceptions import ValidationError, ObjectDoesNotExist
+from django.core.management.base import BaseCommand
+from django.db import IntegrityError
+
+from googleapiclient.discovery import build
+import os
+
+from course.models.models import QuestionCategory
+from video_recommendations.models import Video
+
+
+def get_videos_by_category_name(category_name, category_id):
+    service = build('youtube', 'v3', developerKey=os.environ['YOUTUBE_API_KEY'])
+
+    search_query = category_name + 'in Java'
+    response = service.search().list(
+        part="id",
+        type='video',
+        q=search_query,
+        order='relevance',
+        maxResults=10).execute()
+
+    videos = []
+    for result in response['items']:
+        videos.append(result['id']['videoId'])
+
+    for video_id in videos:
+        video_data = service.videos().list(
+            part='snippet,statistics,contentDetails',
+            id=video_id,
+            fields='items(snippet,statistics,contentDetails(duration))').execute()
+        video_data = video_data['items'][0]
+
+        title = video_data['snippet'].get('title', '')
+        description = video_data['snippet'].get('description', None)
+        duration = video_data['contentDetails'].get('duration', None)
+        published_at = video_data['snippet'].get('publishedAt', None)
+        view_count = video_data['statistics'].get('viewCount', None)
+        like_count = video_data['statistics'].get('likeCount', None)
+        comment_count = video_data['statistics'].get('commentCount', None)
+        youtube_category = video_data['snippet'].get('categoryId', None)
+        try:
+            associated_category = QuestionCategory.objects.get(description=category_id)
+        except ObjectDoesNotExist:
+            associated_category = None
+
+        try:
+            existing_video = Video.objects.get(video_id=video_id)
+            existing_video.associated_categories.add(associated_category)
+        except ObjectDoesNotExist:
+            try:
+                new_video = Video(video_id=video_id, title=title, description=description, duration=duration,
+                                  publish_date=published_at, view_count=view_count, like_count=like_count,
+                                  comment_count=comment_count, youtube_category=youtube_category)
+                new_video.save()
+                new_video.associated_categories.add(associated_category)
+            except (ValidationError, IntegrityError):
+                pass
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        print('Updating DB with Youtube Videos')
+        sub_categories = QuestionCategory.objects.exclude(parent=None)
+        for subcat in sub_categories:
+            print('Querying ' + subcat.description + '...')
+            get_videos_by_category_name(subcat.description, subcat.pk)
--- video_recommendations/models.py
+++ video_recommendations/models.py
@@ -1,12 +1,20 @@
 from django.db import models
 
 from accounts.models import MyUser
+from course.models.models import QuestionCategory
 
 
 class Video(models.Model):
     video_id = models.CharField(max_length=11, null=False, blank=True, primary_key=True)
     title = models.TextField(db_index=True, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    duration = models.CharField(max_length=50, null=True, blank=False)
+    publish_date = models.CharField(max_length=50, null=True, blank=False)
+    view_count = models.IntegerField(null=False, blank=False, default=0)
+    like_count = models.IntegerField(null=False, blank=False, default=0)
+    comment_count = models.IntegerField(null=False, blank=False, default=0)
+    youtube_category = models.CharField(max_length=50, null=True, blank=False)
+    associated_categories = models.ManyToManyField(QuestionCategory)
 
     def __str__(self):
         return self.video_id
